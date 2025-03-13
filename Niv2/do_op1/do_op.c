ced6b0a3fb7c07c7329814b0a5506aded56e9ad4
cmetee-b <claudinebitee@gmail.com>
2025-03-13 23:32:09 +0100

    update sucess

--- Niv2/do_op1/do_op.c
+++ Niv2/do_op1/do_op.c
@@ -5,30 +5,29 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: asiatik <asiatik@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/13 21:19:32 by asiatik           #+#    #+#             */
-/*   Updated: 2025/03/13 21:37:24 by asiatik          ###   ########.fr       */
+/*   Created: 2025/02/12 22:42:09 by asiatik           #+#    #+#             */
+/*   Updated: 2025/02/12 23:15:16 by asiatik          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
 
-int main(int ac, char **av)
+int	main(int ac, char **av)
 {
-    if (ac == 4)
-    {
-        if (av[2][0] == '+')
-            printf("%d", (atoi(av[1]) + atoi(av[3])));
-        if (av[2][0] == '-')
-            printf("%d", (atoi(av[1]) - atoi(av[3])));
-        if (av[2][0] == '*')
-            printf("%d", (atoi(av[1]) * atoi(av[3])));
-        if (av[2][0] == '/')
-            printf("%d", (atoi(av[1]) / atoi(av[3])));
-        if (av[2][0] == '%')
-            printf("%d", (atoi(av[1]) % atoi(av[3])));
-    }
-    printf("\n");
-    return (0);
+	if (ac == 4)
+	{
+		if (av[2][0] == '+')
+			printf("%d", (atoi(av[1]) + atoi(av[3])));
+		if (av[2][0] == '-')
+			printf("%d", (atoi(av[1]) - atoi(av[3])));
+		if (av[2][0] == '*')
+			printf("%d", (atoi(av[1]) * atoi(av[3])));
+		if (av[2][0] == '/')
+			printf("%d", (atoi(av[1]) / atoi(av[3])));
+		if (av[2][0] == '%')
+			printf("%d", (atoi(av[1]) % atoi(av[3])));
+	}
+	printf("\n");
+	return (0);
 }
